0e40009735642a43edf900e339a9f2a134fa27bf
Fabrizio Antoniutti <bitofvg@gmail.com>
2020-12-11 11:36:11 +0100

    Add project files.

---
+++ IdServer/Config.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdServer {
+  public static class Config {
+    public static IEnumerable<IdentityResource> IdentityResources =>
+               new IdentityResource[]
+               {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+               };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+                new ApiScope("scope1"),
+                new ApiScope("scope2"),
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+
+          // Clent Blazor #1
+          new Client
+          {
+            ClientId = "BlazorCli1",
+            AllowedGrantTypes = GrantTypes.Code,
+            RequirePkce = true,
+            RequireClientSecret = false,
+            AllowedCorsOrigins = { "https://localhost:5001" },
+            AllowedScopes = { "openid", "profile" },
+            RedirectUris = { "https://localhost:5001/authentication/login-callback" },
+            PostLogoutRedirectUris = { "https://localhost:5001/" },
+            Enabled = true
+          },
+
+        };
+  }
+}
